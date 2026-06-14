c2fe654a12181caa01d1eea8a4984b16519a56a4
Erik Rybtke <tillykke.nu@iMac-tilhrende-Erik.local>
2026-06-14 11:35:54 +0200

    Fix Netlify function config

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -1,7 +1,10 @@
 exports.handler = async () => {
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       anonKey: process.env.SUPABASE_ANON_KEY
     })
