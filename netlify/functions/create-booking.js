260402835f3e510261fa0da720aa967d1a4064e7
Erik Rybtke <tillykke.nu@iMac-tilhrende-Erik.local>
2026-06-17 11:17:40 +0200

    Connect Stripe checkout to booking creation

--- netlify/functions/create-booking.js
+++ netlify/functions/create-booking.js
@@ -214,7 +214,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ success: true, email: emailData })
+      body: JSON.stringify({ success: true, email: emailData, bookingId: booking.id })
     };
 
   } catch (e) {
